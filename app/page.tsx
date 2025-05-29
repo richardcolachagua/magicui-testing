c97ecb567a0934a1754dccad57403881a8ca54d5
Richard Colachagua <richardcolachagua@gmail.com>
2025-05-29 16:37:23 -0400

    Understanding tailwind and how to add styling and classes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,29 @@
 import { TextAnimate } from "@/components/magicui/text-animate";
+import { PulsatingButton } from "@/components/magicui/pulsating-button";
+import { VideoText } from "@/components/magicui/video-text";
 
 export default function Home() {
   return (
     <div>
-      <TextAnimate animation="scaleUp" by="text">
-        Scale up by text
+      <TextAnimate
+        as="h1"
+        className="mx-auto flex max-w-sm items-center rounded-xl font-bold text-5xl"
+      >
+        Choose Your Fate
       </TextAnimate>
+      <div className="relative h-[200px] w-full overflow-hidden">
+        <VideoText src="https://cdn.magicui.design/ocean-small.webm">
+          MATRIX
+        </VideoText>
+      </div>
+      <div className="flex justify-center gap-4 mt-6">
+        <PulsatingButton className="text-center font-bold text-2xl bg-red-500">
+          Red Pill
+        </PulsatingButton>
+        <PulsatingButton className="text-center font-bold text-2xl bg-blue-500">
+          Blue Pill
+        </PulsatingButton>
+      </div>
     </div>
   );
 }
